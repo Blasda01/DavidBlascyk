0ec66127b19974972c986380189916dcf928b1e6
David Blascyk <dblascyk@prairie-care.com>
2023-08-14 12:56:37 -0500

    new intro

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,2 @@
-aboutMeText = "David Blascyk is my name, and Data Analytics is my game. By game, I mean gain....ful employment. Please look around at my resume, projects, and other examples and marvel at my data-loving skills"
+aboutMeText = "David Blascyk is my name, and Software & Data are my game. By game, I mean gain....ful employment. Please look around at my resume, projects, and other examples and marvel at my data-loving skills"
 
